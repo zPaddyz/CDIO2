031d7e46226feb8ace47bea857d07d65222960bb
[zPaddyz] <mian19ah@student.cbs.dk>
2020-10-15 11:34:38 +0200

    Tilføjet player klasse fra cdio 1

---
+++ src/Game/Player.java
@@ -0,0 +1,43 @@
+package Game;
+
+/* *************************************************************************************************************** */
+// Titel: CDIO 1 - Terningespil
+// Group: 34                                                                                                             *
+// Medlemmer:                                                                                                             *
+// Sebastian Andreas Almfort        - s163922                                                                                                              *
+// Lucas Arleth Lykke               - s205447
+// Mike Patrick Nørlev Andersen     - s205417
+// Martin Koch                      - s182935
+// Anne Sophie Bondegaard Petersen  - s194582
+// Jan Engers Møller Pedersen       - s205419
+/* *************************************************************************************************************** */
+
+public class Player {
+    //point is used to manage our players score
+    private int point = 0;
+    //the hasTurn boolean is used to determine which players turn it is.
+    boolean hasTurn = false;
+    //the Name string is used to give the players their own chosen name.
+    public String Name;
+
+    //this player method is used to instantiate a new player
+    public Player(){
+
+    }
+    //we use this method to change between each players turn
+    public void setHasTurn(boolean hasTurn) {
+        this.hasTurn = hasTurn;
+    }
+    //setPoint is used to add points to the players score
+    public void setPoint(int point) {
+        // if the player roll 2 i.e two 1s then the player loses all his/her points
+        if(point != 2)  this.point += point;
+        else this.point = 0;
+
+    }
+    //getPoint returns point and is required because point is set to private
+    public int getPoint() {
+        return point;
+    }
+
+}
