db6677d23d0abb2687945cb62c828dac5cfbc7cb
SAA-DK <70898210+SAA-DK@users.noreply.github.com>
2020-10-15 11:53:48 +0200

    Create Field.java

---
+++ src/Game/Field.java
@@ -0,0 +1,55 @@
+public class Field
+{
+
+    public static void main(String[] args)
+    {
+        int n = 11;
+
+        switch (n)
+        {
+            case 2:
+                System.out.println("You climb the highest tower in the country, and at the top you find 250");
+                break;
+
+            case 3:
+                System.out.println("You fell in a crater and have to pay to get help to get out. You pay 100");
+                break;
+
+            case 4:
+                System.out.println("You stand in front of the palace gates and find a gold coin. You sell it for 100");
+                break;
+
+            case 5:
+                System.out.println("You have ended up in a cold desert. To keep warm you by a blanket for 20");
+                break;
+
+            case 6:
+                System.out.println("You have helped building a walled city, and as pay you get 180");
+                break;
+
+            case 7:
+                System.out.println("It is getting very late, so you find a monostary where you can stay overnight for free");
+                break;
+
+            case 8:
+                System.out.println("You have gotten lost in a black cave. To get out you use your lighter that you later will have to refill for 70");
+                break;
+
+            case 9:
+                System.out.println("You have helped building huts in the mountains. For your work you get 60");
+                break;
+
+            case 10:
+                System.out.println("???");
+                break;
+
+            case 11:
+                System.out.println("You have fallen down the pit. in you fall you lost 50");
+                break;
+
+            case 12:
+                System.out.println("You have inherited a goldmine that will make rich very fast. Till a start you get 650");
+                break;
+        }
+    }
+}
