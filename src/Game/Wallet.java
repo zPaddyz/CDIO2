b8092494c7aed570b58b0ce63eeed45f9c773a16
[zPaddyz] <mian19ah@student.cbs.dk>
2020-10-15 13:07:17 +0200

    Tilføjet < 0 krav

--- src/Game/Wallet.java
+++ src/Game/Wallet.java
@@ -11,6 +11,9 @@ public class Wallet {
     public void setPoint(int point) {
         // if the player roll 2 i.e two 1s then the player loses all his/her points
         this.point += point;
+        if(point < 0) {
+            point = 0;
+        }
     }
     //getPoint returns point and is required because point is set to private
     public int getPoint() {
