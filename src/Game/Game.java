141556655a79bf7668e146fd2169ad9d8cd8edcd
Martin <s182935@student.dtu.dk>
2020-10-18 11:09:10 +0200

    Kommenteret kode i game flow

--- src/Game/Game.java
+++ src/Game/Game.java
@@ -10,9 +10,9 @@ public class Game {
         Dice d2 = new Dice(1);
         Field f1 = new Field();
         System.out.println("Please enter the amount of sides you wish your dice to have: ");
-        d1.setMAX(scan.nextInt());
+        d1.setMAX(scan.nextInt()); //initiates how many sides the dices in the game should have.
         d2.setMAX(d1.getMAX());
-        Player p1 = new Player();
+        Player p1 = new Player(); //player object
         Player p2 = new Player();
         System.out.println("Enter player 1's name: ");
         p1.Name = scan.next();
@@ -23,21 +23,21 @@ public class Game {
         while (true) {
             if (p1.hasTurn) {
                 System.out.println(p1.Name + " Enter: 'roll' to roll the dice");
-                if (scan.next().toLowerCase().equals("roll")) {
-                    dicevalue = d1.rollDice() + d2.rollDice();
-                    if (dicevalue > 12) {
-                        dicevalue = dicevalue % 12;
+                if (scan.next().toLowerCase().equals("roll")) { //write roll into console to throw dices.
+                    dicevalue = d1.rollDice() + d2.rollDice(); //checking for sum of dices.
+                    if (dicevalue > 12) { //it is possible to have an unlimited dice value, but the board only has 12 fields.
+                        dicevalue = dicevalue % 12; //example: you roll 40 which is the same as rolling a 4. (When you reach the limit on the board you go back to field number 2).
                     }
                     System.out.println(p1.Name + " Rolls: " + dicevalue);
-                    f1.fieldswitch(dicevalue);
-                    p1.wallet.setPoint(f1.Money);
+                    f1.fieldswitch(dicevalue); //calls what field you landed on with a sentence.
+                    p1.wallet.setPoint(f1.Money); //setting your points according to the field you landed on.
                     System.out.println(p1.Name + " Points: " + p1.wallet.getPoint());
                     if (p1.wallet.getPoint() >= 3000) {
                         System.out.println("You win!");
                         System.out.println("End of game.");
                         break;
                     }
-                    if (dicevalue != 10) {
+                    if (dicevalue != 10) { //extra turn if you roll 10
                         p1.hasTurn = false;
                     }
                 }
